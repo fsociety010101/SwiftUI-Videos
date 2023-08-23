db1938e843ad91ab391db0583ff2dd76c0bfcae6
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-08-23 15:57:37 +0200

    Initial Commit

---
+++ SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Dmytro Nimchynskyi on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
